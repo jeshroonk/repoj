01f6508eabef5d5028521ca170c0954d299f0d10
veeres-javali18 <veeresjavali038@gmail.com>
2025-04-09 04:51:21 +0530

    second java file

---
+++ ar.java
@@ -0,0 +1,12 @@
+class args{
+    public static void main(String[]args)
+    {
+        int a=10;
+        int b=3;
+        System.out.println("a+b="+(a+b));
+        System.out.println("a-b="+(a-b));
+        System.out.println("a*b="+(a*b));
+        System.out.println("a/b="+(a/b));
+        System.out.println("a%b="+(a%b));
+    }
+}
